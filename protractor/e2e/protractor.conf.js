caed526cc2fcb55b8f01ca5cf8d99969fd84cb9c
cdaza <cdaza933@hotmail.com>
2020-09-06 14:48:55 -0500

    protractor data

--- protractor/e2e/protractor.conf.js
+++ protractor/e2e/protractor.conf.js
@@ -1,7 +1,3 @@
-// @ts-check
-// Protractor configuration file, see link for more information
-// https://github.com/angular/protractor/blob/master/lib/config.ts
-
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 /**
@@ -13,7 +9,10 @@ exports.config = {
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    chromeOptions: {
+      args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
+    }
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
